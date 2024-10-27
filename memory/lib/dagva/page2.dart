cabdf4469e0a28a5f7702add3b90c2f778c38ff3
Dagva2232 <144753275+Dagva2232@users.noreply.github.com>
2024-10-27 21:24:56 +0800

    Update page2.dart

--- memory/lib/dagva/page2.dart
+++ memory/lib/dagva/page2.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class PaginationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'Хуудсууд нь виртуал санах ойн чухал хэсэг юм. Энэ нь физик санах ой болон виртуал санах ойг тогтмол хэмжээтэй хуудас болгон хуваах явдал юм. ',
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page10'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Хуудсууд нь виртуал санах ойн чухал хэсэг юм. Энэ нь физик санах ой болон виртуал санах ойг тогтмол хэмжээтэй хуудас болгон хуваах явдал юм.',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
