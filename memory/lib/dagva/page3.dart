b7ad63dd8f1103535230f55ed9417491633e6a59
Dagva2232 <144753275+Dagva2232@users.noreply.github.com>
2024-10-27 21:25:26 +0800

    Update page3.dart

--- memory/lib/dagva/page3.dart
+++ memory/lib/dagva/page3.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class PageReplacementPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'Хуудас солих алгоритмуудад санах ой дүүрсэн үед аль хуудсыг солихыг систем шийддэг.',
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page11'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Хуудас солих алгоритмуудад санах ой дүүрсэн үед аль хуудсыг солихыг систем шийддэг.',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
