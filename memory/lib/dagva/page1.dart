35263666d020a8a901b946dc62447ee5be670ef3
Dagva2232 <144753275+Dagva2232@users.noreply.github.com>
2024-10-27 21:14:20 +0800

    Update page1.dart

--- memory/lib/dagva/page1.dart
+++ memory/lib/dagva/page1.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-class VirtualMemoryPage extends StatelessWidget {
+class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'Виртуал санах ой нь орчин үеийн компьютерийн системүүдийн гол ойлголт бөгөөд програмууд нь физикээ санах ойгоос илүү санах ойг ашиглах боломжийг олгодог.',
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 9'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Виртуал санах ой нь орчин үеийн компьютерийн системүүдийн гол ойлголт бөгөөд програмууд нь физикээ санах ойгоос илүү санах ойг ашиглах боломжийг олгодог.',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
