88b3ad1b2c834f41d8e3d0fab0c3b999607e8629
Dagva2232 <144753275+Dagva2232@users.noreply.github.com>
2024-10-27 20:53:16 +0800

    Add files via upload

---
+++ page4.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OptimalAlgorithmPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          'Саяхан ашиглагдаагүй хуудас солих алгоритмнь практик хувилбарыг санал болгож өмнө нь хандсан хуудсууд удахгүй дахин нэвтэрч магадгүй гэсэн таамаглал дээр үндэслэн саяхан ашиглагдаагүй хуудсыг устгадаг.',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
