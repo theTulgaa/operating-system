612c8318f86c19374952b067850c9b171c188838
Dagva2232 <144753275+Dagva2232@users.noreply.github.com>
2024-10-27 21:25:45 +0800

    Update page4.dart

--- memory/lib/dagva/page4.dart
+++ memory/lib/dagva/page4.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class OptimalAlgorithmPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'Саяхан ашиглагдаагүй хуудас солих алгоритмнь практик хувилбарыг санал болгож өмнө нь хандсан хуудсууд удахгүй дахин нэвтэрч магадгүй гэсэн таамаглал дээр үндэслэн саяхан ашиглагдаагүй хуудсыг устгадаг.',
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page12'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Саяхан ашиглагдаагүй хуудас солих алгоритм нь практик хувилбарыг санал болгож өмнө нь хандсан хуудсууд удахгүй дахин нэвтэрч магадгүй гэсэн таамаглал дээр үндэслэн саяхан ашиглагдаагүй хуудсыг устгадаг.',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
